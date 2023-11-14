6ce97dfaaea8c0bfae052363c2c7998b0a4ba80a
kukuh-imanura <kukuh.imanura.bagaskara@gmail.com>
2023-11-14 18:42:23 +0800

    add shadcn ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { Button } from "@/components/ui/button"
+
 // Tailwind
 export default function App() {
   return (
     <>
+      {/* DEFAULT */}
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
+
+      {/* DAISY UI */}
       <button class="btn">Button</button>
+
+      {/* SHADCN UI */}
+      <Button>Click me</Button>
     </>
     
   )
