d49766d6acdd099eb9ce0bc9355168a532ac12e3
kukuh-imanura <kukuh.imanura.bagaskara@gmail.com>
2023-11-14 18:48:10 +0800

    test daisy ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 // Tailwind
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      {/* DEFAULT */}
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+
+      {/* DAISY UI */}
+      <button className="btn">Button</button>
+    </>
+    
   )
 }
 
