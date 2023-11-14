d04e432093ad872c81cf70010fec356266ccbdf4
kukuh-imanura <kukuh.imanura.bagaskara@gmail.com>
2023-11-14 18:44:16 +0800

    Revert "add daisy ui"
    This reverts commit 5de5a526b8d0c54160fe704ffca7c9afc19eab9c.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 // Tailwind
 export default function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <button class="btn">Button</button>
-    </>
-    
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
 
