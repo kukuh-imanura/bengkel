5de5a526b8d0c54160fe704ffca7c9afc19eab9c
kukuh-imanura <kukuh.imanura.bagaskara@gmail.com>
2023-11-14 17:53:36 +0800

    add daisy ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 // Tailwind
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <button class="btn">Button</button>
+    </>
+    
   )
 }
 
